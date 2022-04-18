ae0807801cc4f329fed2b2492336e5abd9594f87
Nance <73056456+Nance-the-First@users.noreply.github.com>
2022-04-18 17:07:37 +0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,45 +1,37 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
-
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders')
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('stats')
-    .setDescription('Get all stats for a user')
-    .setDescription('Info about a user')
-    .addUserOption((option) =>
-      option.setName('target').setDescription('The user')
-    ),
-  execute: async (interaction) => {
-    const user = interaction.options.getUser('target')
-      ? interaction.options.getUser('target')
-      : interaction.user
-
-    console.log('Execute', user.tag)
-
-    const knex = interaction.knex
-
-    const last = await knex('avatar')
-      .select('url', 'changed_at')
-      .where({ user_id: user.id })
-      .orderBy('id', 'desc')
-      .first()
-
-    if (!last)
-      return await interaction.reply({
-        content: "This user didn't change his avatar recently",
-        ephemeral: true,
-      })
-
-    const stats = await knex('avatar')
-      .select('type', 'type')
-      .where({ user_id: user.id })
-      .count('type as count')
-
-    const output =
-      stats.map((e) => `${e.type}: ${e.count}`).join('\n') +
-      `\nLast Avatar: ${last.url}\nChanged: <t:${Math.floor(
+    data: {
+        "name": "stats",
+        "description": "Get all stats for a user",
+        "options": [
+        {
+            "type": 6,
+            "name": "target",
+            "description": "Info about a user"
+        }]
+    },
+    execute: async (interaction) => {
+        const user = interaction.options.getUser('target') ? interaction.options.getUser('target') : interaction.user
+        console.log('Execute', user.tag)
+        const knex = interaction.knex
+        const last = await knex('avatar').select('url', 'changed_at').where({
+            user_id: user.id
+        }).orderBy('id', 'desc').first()
+        if (!last) return await interaction.reply({
+            content: "This user didn't change his avatar recently",
+            ephemeral: true,
+        })
+        const stats = await knex('avatar').select('type', 'type').where({
+            user_id: user.id
+        }).count('type as count')
+        const output = stats.map((e) => `${e.type}: ${e.count}`).join('\n') + `\nLast Avatar: ${last.url}\nChanged: <t:${Math.floor(
         last.changed_at / 1000
       )}:R>`
-
-    await interaction.reply({ content: output, ephemeral: true })
-  },
+        await interaction.reply({
+            content: output,
+            ephemeral: true
+        })
+    },
 }
